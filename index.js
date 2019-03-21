ffb359684a069790910799b06188daa86bcc3b84
Zech Drinkhall <zechdrink@aol.com>
2019-03-21 12:26:26 -0700

    .

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING;
 
-server.listen(5000, () => {
+server.listen(port, () => {
   console.log(`\n* ${greeting}, Server Running on http://localhost:${port} *\n`);
 });
